b0b1563ab5406356d00098cd5dcb2cf5f3fc88b2
tuan2004a <bi03112004@gmail.com>
2025-09-11 22:23:13 +0700

    style: inforAgents.jsx

--- src/components/container/InforAgents.jsx
+++ src/components/container/InforAgents.jsx
@@ -14,7 +14,7 @@ const InforAgents = () => {
 				{agentDetailById?.name}
 			</h1>
 			<div className={`${agentDetailById?.color?.textColor || []} ${agentDetailById?.color?.borderColor || []} flex items-start space-x-5 p-5`}>
-				<img className="size-11 rounded-full block shrink-0" src="#" alt="" />
+				<img className="size-11 rounded-full block shrink-0" src={`${agentDetailById?.role?.img || []}`} alt="" />
 				<div className="">
 					<h5 className="text-base mb-1.5">{agentDetailById?.role?.role || []}</h5>
 					<p className="text-xs leading-normal inline-block">A phantom of a memory, Omen hunts in the shadows. He renders enemies blind, teleports across the field, then lets paranoia take hold as his foe scrambles to learn where he might strike next.</p>
@@ -24,19 +24,19 @@ const InforAgents = () => {
 			{/* skill */}
 			<div className={`${agentDetailById?.color?.textColor || []} skill grid grid-cols-4 mt-3 gap-3 uppercase font-medium`}>
 				<div className={`${agentDetailById?.color?.borderColor || []}  w-full flex flex-col justify-center items-center`}>
-					<img className="w-[75%] py-2" src="/skill/Viper-PoisonCloud-Q.webp" alt="" />
+					<img className="w-[75%] py-2" src={`${agentDetailById?.skill?.Q?.img || []}`} alt="" />
 					<p className="w-full text-center leading-none py-1 border-t">Q</p>
 				</div>
 				<div className={`${agentDetailById?.color?.borderColor || []} w-full flex flex-col justify-center items-center`}>
-					<img className="w-[75%] py-2" src="/skill/Viper-ToxicScreen-E.webp" alt="" />
+					<img className="w-[75%] py-2" src={`${agentDetailById?.skill?.E?.img || []}`} alt="" />
 					<p className="w-full text-center leading-none py-1 border-t">e</p>
 				</div>
 				<div className={`${agentDetailById?.color?.borderColor || []} w-full flex flex-col justify-center items-center`}>
-					<img className="w-[75%] py-2" src="/skill/Viper-SnakeBite-C.webp" alt="" />
+					<img className="w-[75%] py-2" src={`${agentDetailById?.skill?.C?.img || []}`} alt="" />
 					<p className="w-full text-center leading-none py-1 border-t ">c</p>
 				</div>
 				<div className={`${agentDetailById?.color?.borderColor || []} w-full flex flex-col justify-center items-center`}>
-					<img className="w-[75%] py-2" src="/skill/Viper-ViperPit-X.webp" alt="" />
+					<img className="w-[75%] py-2" src={`${agentDetailById?.skill?.X?.img || []}`} alt="" />
 					<p className="w-full text-center leading-none py-1 border-t ">x</p>
 				</div>
 			</div>
--- src/components/container/ListAgents.jsx
+++ src/components/container/ListAgents.jsx
@@ -12,12 +12,12 @@ const ListAgents = ({ itemsData }) => {
 
 	return (
 		<div onClick={handleSelectId} className="ListAgents flex flex-col items-end pr-5 mb-7 cursor-pointer">
-			<div className=" relative overflow-hidden after:absolute after:w-full after:h-0.5 after:bg-[#038C36] after:z-50 after:bottom-0 before:absolute before:w-[88%] before:h-0.5 before:bg-[#038C36] before:z-50 before:bottom-1 before:left-1/2 before:-translate-x-1/2">
-				<div className=" z-10">
+			<div className="relative overflow-hidden after:absolute after:w-full after:h-0.5 after:bg-[#038C36] after:z-50 after:bottom-0 before:absolute before:w-[88%] before:h-0.5 before:bg-[#038C36] before:z-50 before:bottom-1 before:left-1/2 before:-translate-x-1/2">
+				<div className="">
 					<img className={`${itemsData.name} absolute size-15 overflow-hidden z-10 top-1/2 -translate-x-1/2 left-1/2 w-full h-full object-top object-cover`} src={`${itemsData.imgAgents}`} alt="" />
 				</div>
 
-				<div className={`${agentDetailById?.color?.textColor || []} z-20`}>
+				<div className={`${agentDetailById?.color?.textColor || []}`}>
 					<svg width="60" height="60" viewBox="0 0 68 68"  fill="none" xmlns="http://www.w3.org/2000/svg">
 						<path d="M63 5V63H5V22.459L25.3691 5H63Z" className="stroke-current" strokeWidth="2" />
 						<path d="M67 1V67H1V20.8594L24.1699 1H67Z" className="stroke-current" strokeWidth="2" />
